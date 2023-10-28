c057be27305bb86111860591dea008a0be9b09cf
Abdul Wahid <wahidshaik0646@gmaiil.com>
2023-10-28 10:23:34 +0530

    Expense Tracker Project

---
+++ public/js/login.js
@@ -0,0 +1,46 @@
+const signUp = document.getElementById("signUp");
+const signIn = document.getElementById("signIn");
+const container = document.getElementById("container");
+const signUpBtn = document.getElementById("signUpBtn");
+const loginBtn = document.getElementById("loginBtn");
+const loginEmail = document.getElementById("loginEmail");
+const loginPassword = document.getElementById("loginPassword");
+
+signUp.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+signIn.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
+const spans = document.querySelectorAll('.ap');
+
+spans.forEach(span => {
+  console.log(span.dataset);
+  console.log(span.dataset.mentionValue);
+});
+
+function login() {
+  const loginDetails = {
+    loginEmail: loginEmail.value,
+    loginPassword: loginPassword.value,
+  };
+
+  axios
+    .post("http://localhost:3000/user/login", loginDetails)
+    .then((result) => {
+      alert(result.data.message);
+      localStorage.setItem("token", result.data.token);
+      window.location.href = "/homePage";
+    })
+    .catch((error) => {
+      if (error.response) {
+        const errorMessage = error.response.data.message;
+        alert(errorMessage);
+      } else {
+        alert("An error occurred. Please try again later.");
+      }
+    });
+}
+
+loginBtn.addEventListener("click", login);
